142b77630184624116f400e9660ceb22c4b9ce4d
Brendon Henning <brendonhenning@gmail.com>
2020-10-06 17:22:54 -0500

    rebuilt without scaffold

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,76 +1,50 @@
 class KittensController < ApplicationController
   before_action :set_kitten, only: [:show, :edit, :update, :destroy]
 
-  # GET /kittens
-  # GET /kittens.json
   def index
     @kittens = Kitten.all
   end
 
-  # GET /kittens/1
-  # GET /kittens/1.json
-  def show
-    @kitten = Kitten.find(params[:id])
-  end
-
-  # GET /kittens/new
   def new
     @kitten = Kitten.new
   end
 
-  # GET /kittens/1/edit
-  def edit
-    @kitten = Kitten.find(params[:id])
-  end
-
-  # POST /kittens
-  # POST /kittens.json
   def create
     @kitten = Kitten.new(kitten_params)
-
-    respond_to do |format|
-      if @kitten.save
-        format.html { redirect_to @kitten, notice: 'Kitten was successfully created.' }
-        format.json { render :show, status: :created, location: @kitten }
-      else
-        format.html { render :new }
-        format.json { render json: @kitten.errors, status: :unprocessable_entity }
-      end
+    if @kitten.save
+      redirect_to kitten_path(@kitten.id), notice: 'New Kitten successfully created'
+    else
+      @kitten = Kitten.new(kitten_params)
     end
   end
 
-  # PATCH/PUT /kittens/1
-  # PATCH/PUT /kittens/1.json
+  def show
+  end
+
+  def edit
+  end
+
   def update
-    respond_to do |format|
-      if @kitten.update(kitten_params)
-        format.html { redirect_to @kitten, notice: 'Kitten was successfully updated.' }
-        format.json { render :show, status: :ok, location: @kitten }
-      else
-        format.html { render :edit }
-        format.json { render json: @kitten.errors, status: :unprocessable_entity }
-      end
+    if @kitten.update(kitten_params)
+      redirect_to kitten_path(@kitten.id), notice: 'Kitten successfully updated'
+    else
+      @kitten = Kitten.edit
     end
   end
 
-  # DELETE /kittens/1
-  # DELETE /kittens/1.json
   def destroy
     @kitten.destroy
-    respond_to do |format|
-      format.html { redirect_to kittens_url, notice: 'Kitten was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to kittens_path, notice: 'Kitten killed. :('
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_kitten
-      @kitten = Kitten.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def kitten_params
-      params.require(:kitten).permit(:name, :age, :softness, :cuteness)
-    end
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
 end
